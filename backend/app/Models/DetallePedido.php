9d8e83a937aae51f0909af6cf0378d92e6b52267
unknown <diegoselayamolina@gmail.com>
2026-06-01 10:56:39 -0400

    saving changes

--- backend/app/Models/DetallePedido.php
+++ backend/app/Models/DetallePedido.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class DetallePedido extends Model
 {
     protected $table = 'detalle_pedido';
-    protected $primaryKey = 'id_detalle_pedido'; // Tu script usa este nombre
+    protected $primaryKey = 'id_detalle_pedido';
     public $timestamps = false;
 
     protected $fillable = [
